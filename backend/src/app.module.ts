c51b4dc6279c1d7c5fc7085f988c692787a78666
IsmaCar <carballomartinismael@gmail.com>
2025-12-23 13:50:43 +0100

    Add Prisma & ClientsModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule, ClientsModule],
   controllers: [],
   providers: [],
 })
