d4106045ab01ad1dbcdad7ff4a41664678c969d8
IsmaCar <carballomartinismael@gmail.com>
2025-12-26 21:36:38 +0100

    Add test @Post

--- backend/src/clients/clients.controller.spec.ts
+++ backend/src/clients/clients.controller.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientsController } from './clients.controller';
+import { ClientsService } from './clients.service';
 
 describe('ClientsController', () => {
   let controller: ClientsController;
+  let mockClientsService: any;
 
   beforeEach(async () => {
+    // Mock del service
+    mockClientsService = {
+      create: jest.fn(),
+      findMany: jest.fn(),
+      findOne: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ClientsController],
+      providers: [
+        {
+          provide: ClientsService,
+          useValue: mockClientsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ClientsController>(ClientsController);
@@ -14,5 +28,13 @@ describe('ClientsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call service.create whit correct params', () => {
+    const dto = { name: 'Juan', email: 'a@a.com', phone: '987654321' };
+
+    controller.create(dto);
+
+    expect(mockClientsService.create).toHaveBeenCalledWith('hardcoded-garage-id', dto);
   });
 });
