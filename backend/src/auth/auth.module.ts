1a52133188ba70a1c1d496eaaac5f19740002eeb
IsmaCar <carballomartinismael@gmail.com>
2026-01-17 15:09:36 +0100

    Import JWT and configurate it

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    //Importar jwt modules con la configuración
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '12h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
