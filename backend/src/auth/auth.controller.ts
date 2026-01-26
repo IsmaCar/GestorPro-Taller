32ea179ad48872bd031c58db0f89d8a27abcbf83
IsmaCar <carballomartinismael@gmail.com>
2026-01-26 19:53:07 +0100

    Create endpoint changePassword

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, UseGuards, Patch, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterTenantDto } from './dto/register-tenant.dto';
 import { LoginOwnerDto } from './dto/login-owner.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { ChangePasswordDto } from './dto/change-password.dto';
+import { UserId } from './decorators/user-id.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -13,8 +16,19 @@ export class AuthController {
     return this.authService.registerTenant(registerTenantDto);
   }
 
+  @HttpCode(200)
   @Post('login-owner')
   loginOwner(@Body() loginOwnerDto: LoginOwnerDto) {
     return this.authService.loginOwner(loginOwnerDto);
+  }
+
+  @Patch('password/change')
+  @UseGuards(JwtAuthGuard)
+  changePassword(
+    @UserId() userId: string,
+    @Request() req: any,
+    @Body() changePasswordDto: ChangePasswordDto,
+  ) {
+    return this.authService.changePassword(userId, changePasswordDto);
   }
 }
