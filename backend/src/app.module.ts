6fc9502eba3d6075ef96a49102515b2b5434260b
IsmaCar <carballomartinismael@gmail.com>
2026-02-19 11:48:20 +0100

    Import EmailModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ClientsModule } from './clients/clients.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ClientsModule],
+  imports: [AuthModule, PrismaModule, ClientsModule, EmailModule],
   controllers: [],
   providers: [],
 })
