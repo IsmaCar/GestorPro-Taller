2dd12fd24a7723b83f1a208cf9aa127cc6cbe288
IsmaCar <carballomartinismael@gmail.com>
2026-02-23 21:17:56 +0100

    Initialize email service

--- backend/src/email/email.service.ts
+++ backend/src/email/email.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import * as brevo from '@getbrevo/brevo';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  private brevoClient: brevo.BrevoClient;
+  constructor() {
+    const apiKey = process.env.BREVO_API_KEY;
+
+    if (!apiKey) throw new Error('BREVO_API_KEY no está configurada en las variables de entorno');
+
+    this.brevoClient = new brevo.BrevoClient({
+      apiKey: apiKey,
+    });
+  }
+  async sendInvitationEmail(recipientEmail: string, invitationToken: string, garageName: string) {}
+}
