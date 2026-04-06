3919826d257035802e882132b545b2d859488ff3
IsmaCar <carballomartinismael@gmail.com>
2026-04-06 19:03:26 +0200

    Update using ConfigModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ClientsModule } from './clients/clients.module';
 import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ClientsModule, EmailModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, //Cargar variables y hacerlas disponibles
+    }),
+    AuthModule,
+    PrismaModule,
+    ClientsModule,
+    EmailModule,
+  ],
   controllers: [],
   providers: [],
 })
