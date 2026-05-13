02d7a996db2b469509ab9ad5f8d6340ce518c4f1
IsmaCar <carballomartinismael@gmail.com>
2026-05-13 12:18:04 +0200

    Update file with create order path

--- backend/src/work-orders/work-orders.controller.ts
+++ backend/src/work-orders/work-orders.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, UseGuards } from '@nestjs/common';
 import { WorkOrdersService } from './work-orders.service';
+import { CreateWorkOrderDto } from './dto/create-work-order.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UserId } from '../auth/decorators/user-id.decorator';
+import { GarageId } from '../auth/decorators/garage-id.decorator';
 
+@UseGuards(JwtAuthGuard)
 @Controller('work-orders')
 export class WorkOrdersController {
   constructor(private readonly workOrdersService: WorkOrdersService) {}
 
+  @HttpCode(201)
+  @Post()
+  create(
+    @Body() createWorkOrderDto: CreateWorkOrderDto,
+    @UserId() userId: string,
+    @GarageId() garageId: string,
+  ) {
+    return this.workOrdersService.create(garageId, userId, createWorkOrderDto);
+  }
+
   @HttpCode(200)
   @Get()
-  findAll() {
-    const garageId = 'hardcoded-garage-id'; //Temporal
+  findAll(@GarageId() garageId: string) {
     return this.workOrdersService.findAll(garageId);
   }
 }
