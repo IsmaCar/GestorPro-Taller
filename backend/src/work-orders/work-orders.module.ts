f93a559a9a2137468f4bf3ba533203d3db305261
IsmaCar <carballomartinismael@gmail.com>
2026-06-18 22:24:28 +0200

    Import Prisma Module

--- backend/src/work-orders/work-orders.module.ts
+++ backend/src/work-orders/work-orders.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WorkOrdersService } from './work-orders.service';
 import { WorkOrdersController } from './work-orders.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [WorkOrdersController],
   providers: [WorkOrdersService],
 })
