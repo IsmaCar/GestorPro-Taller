10f62ab9d6715d93342df2df43dc29da0f1a8074
IsmaCar <carballomartinismael@gmail.com>
2026-06-18 22:23:46 +0200

    Create no token test

--- backend/test/work-order/work-order.e2e-spec.ts
+++ backend/test/work-order/work-order.e2e-spec.ts
@@ -2,6 +2,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../../src/prisma/prisma.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../src/app.module';
+import request from 'supertest';
 
 //Suite de test para el módulo work-orders
 describe('Work-order (e2e)', () => {
@@ -34,9 +35,25 @@ describe('Work-order (e2e)', () => {
   });
 
   afterAll(async () => {
-    // Cerrar la aaplicación y los test al terminarlos
+    // Cerrar la aplicación y los test al terminarlos
     if (app) {
       await app.close();
     }
+  });
+
+  // Verificar entrada autenticada
+  describe('POST /work-orders', () => {
+    it('should return 401 when no token is provided', async () => {
+      const data = {
+        clientId: '550e8400-e29b-41d4-a716-446655440000',
+        vehicleId: '550e8400-e29c-23c4-a718-446655440000',
+        description: 'Test work order',
+        // assignedMechanic omitido (opcional)
+        openingDate: new Date().toISOString(),
+        // el state se asigna automáticamente en el backend, no es necesario enviarlo
+      };
+      const response = await request(app.getHttpServer()).post('/work-orders').send(data);
+      expect(response.status).toBe(401);
+    });
   });
 });
