0358edc804ea856ea8f364479c9a096702716ffd
IsmaCar <carballomartinismael@gmail.com>
2026-08-11 18:39:47 +0200

    Fix class-validator

--- backend/src/work-orders/dto/update-work-order.ts
+++ backend/src/work-orders/dto/update-work-order.ts
@@ -1,16 +1,19 @@
 import { StateOrder } from '@prisma/client';
-import { IsEnum, IsString, IsUUID, Length } from 'class-validator';
+import { IsEnum, IsOptional, IsString, IsUUID, Length } from 'class-validator';
 
 export class UpdateWorkOrderDto {
   @IsString()
   @Length(5, 200)
+  @IsOptional()
   description?: string;
 
   @IsString()
   @IsUUID()
+  @IsOptional()
   assignedMechanic?: string;
 
   @IsString()
   @IsEnum(StateOrder)
+  @IsOptional()
   state?: StateOrder;
 }
